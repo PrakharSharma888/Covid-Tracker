52465d974ae3d4ad1a3f0d3ff783116a57412f28
Prakhar <prakharentp2002@gmail.com>
2021-08-03 19:49:20 +0530

    Updated static

--- Covid/settings.py
+++ Covid/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'en!4!pghtz+!zu_slt^n42g)*dia(c-#)@9rcpsc!0-mx^unfi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['mycovid-19tracker.herokuapp.com']
+ALLOWED_HOSTS = ['mycovid-19tracker.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -119,4 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
